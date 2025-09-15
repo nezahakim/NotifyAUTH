baf617ec225cc76e68a79614db5d746c7ac446b4
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-09-15 11:13:44 +0200

    v.0.1

--- src/routes/api/auth/refresh/+server.ts
+++ src/routes/api/auth/refresh/+server.ts
@@ -1,8 +1,25 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { refreshSession } from '$lib/server/auth';
+import { supabase } from '$lib/server/supabase';
 
 export const POST: RequestHandler = async ({ cookies }) => {
-    const refreshToken = cookies.get('refresh_token');
+    // const refreshToken = cookies.get('refresh_token');
+
+    const auth_token =  cookies.get('auth_token');
+    const auth_token_1 = cookies.get('auth_token_1');
+
+    if(!auth_token || !auth_token_1) {
+        return json({ error: 'Unauthorized - attempt  CODE-C' }, { status: 401 });
+    }
+
+    const { data: refreshData, error} = await supabase
+        .from('auth_sessions')
+        .select('refresh_token')
+        .eq('id', auth_token)
+        .eq('user_id', auth_token_1)
+        .single();
+
+    const refreshToken = refreshData?.refresh_token;
 
     if (!refreshToken) {
         return json({ error: 'Unauthorized - attempt detected' }, { status: 401 });
