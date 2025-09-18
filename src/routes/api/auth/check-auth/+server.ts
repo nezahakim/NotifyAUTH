3889d5fdf98608b97811a32f45cf2a0c20d43856
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-09-18 17:28:01 +0200

    v0.0.1

---
+++ src/routes/api/auth/check-auth/+server.ts
@@ -0,0 +1,29 @@
+import { refreshSession } from "$lib/server/auth";
+import { verifyAccessToken } from "$lib/server/jwt";
+import { json, type RequestHandler } from "@sveltejs/kit";
+
+export const POST: RequestHandler = async ({ cookies }) => {
+    const refreshToken = cookies.get('nc_rt');
+
+    if (!refreshToken) {
+        return json({ authenticated: false, code:"R10" }, { status: 401 });
+    }
+
+    try {
+       const { accessToken } = await refreshSession(refreshToken);
+       
+       const payload = verifyAccessToken(accessToken);
+               return json({
+                   authenticated: true, 
+                   user: {
+                       id: payload.sub,
+                       email: payload.email,
+                       role: payload.role,
+                       sessionId: payload.sessionId
+                   }
+               }, { status: 200 });
+               
+    } catch (err) { 
+        return json({ authenticated: false, error: 'Server error' }, { status: 500 });
+    }
+};
