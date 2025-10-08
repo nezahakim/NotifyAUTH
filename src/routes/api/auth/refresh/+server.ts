ed22eb8515c796d51db45afa675c46d083a152da
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-10-08 15:41:23 +0200

    add delivery

--- src/routes/api/auth/refresh/+server.ts
+++ src/routes/api/auth/refresh/+server.ts
@@ -1,16 +1,22 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { refreshSession } from '$lib/server/auth';
 
-export const POST: RequestHandler = async ({ cookies }) => {
+export const POST: RequestHandler = async ({ cookies, request }) => {
     const refreshToken = cookies.get('nc_rt');
+
+    const authHeader = request.headers.get('Authorization');
+    const token = authHeader?.replace('Bearer ', '');
+
+    const tobe_used = refreshToken || token;
     
-    if (!refreshToken) { 
+    
+    if (!tobe_used){
         return json({ error: 'Unauthorized - attempt detected' }, { status: 401 });
     }
 
     try {
         
-        const { accessToken } = await refreshSession(refreshToken);
+        const { accessToken } = await refreshSession(tobe_used);
         return json({ accessToken });
 
     } catch (error) {
