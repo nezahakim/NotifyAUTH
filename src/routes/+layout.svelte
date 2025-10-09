481d5accd0409986607bdac6146e963f8b6e39b5
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-10-09 17:50:13 +0200

    fix wind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import logo from '$lib/assets/logo-mini.jpg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={logo} />
 </svelte:head>
 
 {@render children?.()}
